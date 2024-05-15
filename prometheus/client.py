2a891366844bf9d7d1d9d58b1f18222b85c34577
Brianaldo <brianaldophandiarta31@gmail.com>
2024-05-15 08:57:21 +0700

    fix: pod count query

--- prometheus/client.py
+++ prometheus/client.py
@@ -46,15 +46,16 @@ class PrometheusClient(object):
     def fetch_pod_count():
         query = \
             """
-            count by (group) (
+            count by(group) (
                 label_replace(
-                    kube_pod_info{
-                        namespace="default",
+                    kube_pod_status_ready{
+                        namespace="default", 
+                        condition="true", 
                         pod=~"^s[0-6].*"
-                    }, 
-                    "group", 
-                    "$1", 
-                    "pod", 
+                    },
+                    "group",
+                    "$1",
+                    "pod",
                     "^([a-z0-9]+)-[a-z0-9]+-[a-z0-9]+$"
                 )
             )
