f8fc894046d99bd6b9b0030aa82027588f0536a6
Brianaldo <brianaldophandiarta31@gmail.com>
2024-06-23 10:59:34 +0700

    feat: update lat-predictor model

--- predictor/lat_predictor/model.py
+++ predictor/lat_predictor/model.py
@@ -6,29 +6,68 @@ import pandas as pd
 
 
 class LatPredictor(object):
-    pipeline = joblib.load(
-        os.path.join(
-            os.path.dirname(__file__), 'preprocessor/lat-tot-v2.joblib'
-        )
-    )
-    model = load_model(
-        os.path.join(os.path.dirname(__file__), 'model/lat-tot-v2.keras')
-    )
+    PIPELINES = None
+    MODELS = None
 
     @staticmethod
     def predict(
         service: str,
         pod: int,
-        cpu: float,
+        cpu_node: float,
+        cpu_pod: str,
         rps: float
     ) -> float:
-        preprocessed_data = LatPredictor.pipeline.transform(
-            pd.DataFrame({
-                'service': [service],
-                'pod': [pod],
-                'cpu': [cpu],
-                'rps': [rps],
-            })
+        input_data = pd.DataFrame({
+            'pod': [pod],
+            'cpu_node': [cpu_node],
+            'cpu_pod': [cpu_pod],
+            'rps': [rps],
+            'rps_per_pod': [rps / pod]
+        })
+
+        input_data[['rps', 'rps_per_pod']] = LatPredictor.PIPELINES['pre'][service].transform(
+            input_data[['rps', 'rps_per_pod']]
         )
 
-        return LatPredictor.model.predict(preprocessed_data)
+        predicted_data = LatPredictor.MODELS[service].predict(
+            input_data
+        )
+
+        return LatPredictor.PIPELINES['post'][service].inverse_transform(
+            predicted_data
+        )[0][0]
+
+    @ staticmethod
+    def load_preprocessor(filename: str):
+        return joblib.load(
+            os.path.join(
+                os.path.dirname(__file__), f"pipelines/{filename}"
+            )
+        )
+
+    @ staticmethod
+    def load_model(filename: str):
+        return load_model(
+            os.path.join(
+                os.path.dirname(__file__), f"models/{filename}"
+            )
+        )
+
+
+if LatPredictor.MODELS is None:
+    LatPredictor.MODELS = {
+        f"s{i}": LatPredictor.load_model(f's{i}.keras')
+        for i in range(7)
+    }
+
+if LatPredictor.PIPELINES is None:
+    LatPredictor.PIPELINES = {
+        "pre": {
+            f"s{i}": LatPredictor.load_preprocessor(f'pre/s{i}.joblib')
+            for i in range(7)
+        },
+        "post": {
+            f"s{i}": LatPredictor.load_preprocessor(f'post/s{i}.joblib')
+            for i in range(7)
+        }
+    }
