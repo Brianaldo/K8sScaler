e04bcaa37bd9d76aebb2a342bafa0c95ce5c32b3
Brianaldo <brianaldophandiarta31@gmail.com>
2024-06-25 22:59:56 +0700

    feat: update model

--- predictor/lat_predictor/model.py
+++ predictor/lat_predictor/model.py
@@ -3,74 +3,82 @@ import os
 from tensorflow.keras.models import load_model
 import joblib
 import pandas as pd
+import numpy as np
 
 import tensorflow as tf
 tf.get_logger().setLevel('ERROR')
 
 
 class LatPredictor(object):
-    PIPELINES = None
-    MODELS = None
+
+    PIPELINE = {
+        'pre': joblib.load(
+            os.path.join(
+                os.path.dirname(__file__), "pipelines/v1/pre.joblib"
+            )
+        ),
+        'post': joblib.load(
+            os.path.join(
+                os.path.dirname(__file__), "pipelines/v1/post.joblib"
+            )
+        )
+    }
+
+    MODEL = load_model(
+        os.path.join(
+            os.path.dirname(__file__), "models/v1.keras"
+        )
+    )
+
+    FEATURE_COLUMN = [
+        f's{i}_pod' for i in range(7)
+    ] + [
+        f's{i}_cpu_pod' for i in range(7)
+    ] + [
+        f's{i}_rps' for i in range(7)
+    ] + [
+        f's{i}_rps_per_pod' for i in range(7)
+    ] + ['cpu_node']
+
+    SCALE_COLUMN = [
+        f's{i}_rps' for i in range(7)
+    ] + [
+        f's{i}_rps_per_pod' for i in range(7)
+    ]
+
+    TARGET_COLUMN = [
+        f's{i}_log_lat' for i in range(7)
+    ]
 
     @staticmethod
     def predict(
-        service: str,
-        pod: int,
-        cpu_node: float,
-        cpu_pod: float,
-        rps: float
+        pod: list[int],
+        cpu_pod: list[int],
+        rps: list[float],
+        rps_per_pod: list[float],
+        cpu_node: int,
     ) -> float:
         input_data = pd.DataFrame({
-            'pod': [pod],
-            'cpu_node': [cpu_node],
-            'cpu_pod': [cpu_pod],
-            'rps': [rps],
-            'rps_per_pod': [rps / pod]
-        })
+            **{f's{i}_pod': pod[i] for i in range(7)},
+            **{f's{i}_cpu_pod': cpu_pod[i] for i in range(7)},
+            **{f's{i}_rps': rps[i] for i in range(7)},
+            **{f's{i}_rps_per_pod': rps_per_pod[i] for i in range(7)},
+            **{cpu_node: cpu_node},
+        }, index=[0])
 
-        input_data[['rps', 'rps_per_pod']] = LatPredictor.PIPELINES['pre'][service].transform(
-            input_data[['rps', 'rps_per_pod']]
+        input_data[LatPredictor.SCALE_COLUMN] = LatPredictor.PIPELINE['pre'].transform(
+            input_data[LatPredictor.SCALE_COLUMN]
         )
 
-        predicted_data = LatPredictor.MODELS[service].predict(
-            input_data, verbose=0
-        )
-
-        return LatPredictor.PIPELINES['post'][service].inverse_transform(
-            predicted_data
-        )[0][0]
-
-    @ staticmethod
-    def load_preprocessor(filename: str):
-        return joblib.load(
-            os.path.join(
-                os.path.dirname(__file__), f"pipelines/{filename}"
+        predicted_data = np.exp(
+            LatPredictor.PIPELINE['post'].inverse_transform(
+                LatPredictor.MODEL.predict(
+                    input_data, verbose=0
+                )
             )
         )
 
-    @ staticmethod
-    def load_model(filename: str):
-        return load_model(
-            os.path.join(
-                os.path.dirname(__file__), f"models/{filename}"
-            )
-        )
-
-
-if LatPredictor.MODELS is None:
-    LatPredictor.MODELS = {
-        f"s{i}": LatPredictor.load_model(f's{i}.keras')
-        for i in range(7)
-    }
-
-if LatPredictor.PIPELINES is None:
-    LatPredictor.PIPELINES = {
-        "pre": {
-            f"s{i}": LatPredictor.load_preprocessor(f'pre/s{i}.joblib')
-            for i in range(7)
-        },
-        "post": {
-            f"s{i}": LatPredictor.load_preprocessor(f'post/s{i}.joblib')
+        return {
+            f's{i}': predicted_data[0][i]
             for i in range(7)
         }
-    }
