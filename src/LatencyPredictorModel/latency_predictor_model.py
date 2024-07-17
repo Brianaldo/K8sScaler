1c7d35a1b8bd5a1bae57735e658bd639299d484c
Brianaldo <brianaldophandiarta31@gmail.com>
2024-07-17 11:42:23 +0700

    feat: update LatencyPredictorModel

--- src/LatencyPredictorModel/latency_predictor_model.py
+++ src/LatencyPredictorModel/latency_predictor_model.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from tensorflow.keras.models import load_model
 import joblib
 import pandas as pd
@@ -30,15 +28,15 @@ class LatencyPredictorModel:
         cpu_node: int,
     ):
         input_data = pd.DataFrame({
-            **{f's{i}_pod': pod[i] for i in range(self.target)},
+            # **{f's{i}_pod': pod[i] for i in range(self.target)},
             **{f's{i}_cpu_pod': cpu_pod[i] for i in range(self.target)},
-            **{f's{i}_rps': rps[i] for i in range(self.target)},
+            # **{f's{i}_rps': rps[i] for i in range(self.target)},
             **{f's{i}_rps_per_pod': rps[i] / pod[i] for i in range(self.target)},
             **{cpu_node: cpu_node},
         }, index=[0])
 
         scale_target = [
-            *[f's{i}_rps' for i in range(self.target)],
+            # *[f's{i}_rps' for i in range(self.target)],
             *[f's{i}_rps_per_pod' for i in range(self.target)]
         ]
         input_data[scale_target] = self.pipelines['pre'].transform(
